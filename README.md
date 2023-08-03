187b70a0c44cab610d20c92f76888b062b4678c1
JhonDoe15 <noamyiz@gmail.com>
2023-08-03 22:01:04 +0300

    added latency metrics, showed relevant metrics by platform, built multiplatform image, updated cli source url

--- README.md
+++ README.md
@@ -11,6 +11,11 @@ run in background `nohup python3 main.py &`
 windows:
 `python3 main.py`
 
+# Development
+
+build to docker hub multiplatform:
+`docker buildx build -t <docker-image-tag> -f <dockerfile-path> --platform linux/amd64,linux/arm64,linux/arm/v7 --push .`
+
 
 exporter base code from https://trstringer.com/quick-and-easy-prometheus-exporter/
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """ookla speedtest exporter"""
 
 import os
+import platform
 from prometheus_client import start_http_server, Gauge
 import time
 import json
@@ -8,6 +9,13 @@ import subprocess
 
 PRINT_SPACING = "################################"
 SOCKET_ERROR = 'Cannot open socket'
+DEFAULT_METRIC_VALUE = 0
+LINUX = False
+if platform.system() == "Linux":
+    print("Running on Linux")
+    LINUX = True
+elif platform.system() == "Windows":
+    print("Running on Windows")
 def retrieve_results():
     speedtest_output = subprocess.run(
                     ["speedtest", "-f", "json"], capture_output=True)
@@ -23,8 +31,16 @@ class AppMetrics:
         self.polling_interval_seconds = polling_interval_seconds
 
         # Prometheus metrics to collect
-        self.download = Gauge("current_download_speed_bits", "Current Download")
-        self.upload = Gauge("current_upload_speed_bits", "Current Upload")
+        self.ping_latency = Gauge("ping_latency_ms", "Current ping latency in milliseconds")
+        self.ping_jitter = Gauge("ping_jitter_ms", "Current ping jitter in milliseconds")
+        if LINUX:
+            self.download_latency = Gauge("download_latency_ms", "Current download latency in milliseconds")
+            self.download_jitter = Gauge("download_jitter_ms", "Current download jitter in milliseconds")
+        self.download_bandwidth = Gauge("download_bandwidth_bytes", "Current Download bandwidth in bytes")
+        if LINUX:
+            self.upload_latency = Gauge("upload_latency_ms", "Current upload latency in milliseconds")
+            self.upload_jitter = Gauge("upload_jitter_ms", "Current upload jitter in milliseconds")
+        self.upload_bandwidth = Gauge("upload_bandwidth_bytes", "Current Upload bandwidth in bytes")
         print("initiated")
 
     def run_metrics_loop(self):
@@ -45,8 +61,17 @@ class AppMetrics:
         json_output = ''
         try:
             json_output = retrieve_results()
-            download_speed_in_bits = json_output.get("download", {}).get("bandwidth", 0) * 8
-            upload_speed_in_bits = json_output.get("upload", {}).get("bandwidth", 0) * 8
+            ping_latency_ms = json_output.get("ping", {}).get("latency", 0)
+            ping_jitter_ms = json_output.get("ping", {}).get("jitter", 0)
+            if LINUX:
+                download_latency_ms = json_output.get("download", {}).get("latency", {}).get("iqm", 0)
+                download_jitter_ms = json_output.get("download", {}).get("latency", {}).get("jitter", 0)
+            download_bandwidth_bytes = json_output.get("download", {}).get("bandwidth", 0) * 8
+            if LINUX:
+                upload_latency_ms = json_output.get("upload", {}).get("latency", {}).get("iqm", 0)
+                upload_jitter_ms = json_output.get("upload", {}).get("latency", {}).get("jitter", 0)
+            upload_bandwidth_bytes = json_output.get("upload", {}).get("bandwidth", 0) * 8
+            print("fetched data!")
         except Exception as e:
             print("exporter had exception:")
             print(json_output)
@@ -58,12 +83,26 @@ class AppMetrics:
                 self.fetch(count)
             
             # default value for if try didnt succeed
-            download_speed_in_bits = 0
-            upload_speed_in_bits = 0
+            ping_latency_ms = DEFAULT_METRIC_VALUE
+            ping_jitter_ms = DEFAULT_METRIC_VALUE
+            download_latency_ms = DEFAULT_METRIC_VALUE
+            download_jitter_ms = DEFAULT_METRIC_VALUE
+            download_bandwidth_bytes = DEFAULT_METRIC_VALUE
+            upload_latency_ms = DEFAULT_METRIC_VALUE
+            upload_jitter_ms = DEFAULT_METRIC_VALUE
+            upload_bandwidth_bytes = DEFAULT_METRIC_VALUE
 
         # Update Prometheus metrics with fetched metrics
-        self.download.set(download_speed_in_bits)
-        self.upload.set(upload_speed_in_bits)
+        self.ping_latency.set(ping_latency_ms)
+        self.ping_jitter.set(ping_jitter_ms)
+        if LINUX:
+            self.download_latency.set(download_latency_ms)
+            self.download_jitter.set(download_jitter_ms)
+        self.download_bandwidth.set(download_bandwidth_bytes)
+        if LINUX:
+            self.upload_latency.set(upload_latency_ms)
+            self.upload_jitter.set(upload_jitter_ms)
+        self.upload_bandwidth.set(upload_bandwidth_bytes)
 
 def main():
     """Main entry point"""
